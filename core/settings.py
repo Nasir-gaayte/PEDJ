4aa2af4b3c6e7ae9083d3e1059d5b1c5434ede97
nasir-gaayte <slowboyz12@gmail.com>
2025-05-01 17:45:01 +0300

    add data

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pedj_db',
+        'USER': 'pedj_user',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

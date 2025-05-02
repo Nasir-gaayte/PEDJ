e4536ea866d3301fd34b4460fb5755093edc9eee
nasir-gaayte <slowboyz12@gmail.com>
2025-05-02 19:42:56 +0300

    collectnew7

--- core/settings.py
+++ core/settings.py
@@ -124,13 +124,12 @@ USE_TZ = True
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type

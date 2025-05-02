b9842ab713604ac67ceb4a75762f1e1df4d5de11
nasir-gaayte <slowboyz12@gmail.com>
2025-05-02 19:11:03 +0300

    collectnew2

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 
 
 
-MEDIA_URL = '/media/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'static/media/'
 STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

bb7c533eec996a2999d741599ff718adfabb1591
nasir-gaayte <slowboyz12@gmail.com>
2025-05-02 19:28:13 +0300

    collectnew5

--- core/settings.py
+++ core/settings.py
@@ -126,9 +126,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [
-    os.path.join(BASE_DIR, 'media'),
-    ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
